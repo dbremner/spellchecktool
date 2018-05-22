18e19533d8c8828ca823f2d89677ab974b7591f1
David Bremner <dbremner@gmail.com>
2018-05-21 19:39:02 -0700

    create a spellchecker and add an autocorrect

--- SpellCheckTool/Program.cs
+++ SpellCheckTool/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SpellCheckTool.Interop;
 
 namespace SpellCheckTool
 {
@@ -10,6 +11,16 @@ namespace SpellCheckTool
     {
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Expected two arguments:");
+                Console.WriteLine("spellchecktool teh the");
+                return;
+            }
+
+            var factory = new SpellCheckerFactory();
+            var checker = factory.CreateSpellChecker("en-us");
+            checker.AutoCorrect(args[0], args[1]);
         }
     }
 }
